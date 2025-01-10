584b5ad28e31f6943398622ead522b28af79ebfd
Vadym Sulim <ohnooq1@gmail.com>
2025-01-10 08:30:55 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 import time
+import asyncio as aio
+from collections.abc import Awaitable
 
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
 from iot.message import Message, MessageType
 from iot.service import IOTService
 
 
-def main() -> None:
+async def run_sequence(*functions: Awaitable) -> None:
+    for function in functions:
+        await function
+
+
+async def run_parallel(*functions: Awaitable) -> None:
+    await aio.gather(*functions)
+
+
+async def main() -> None:
     # create an IOT service
     service = IOTService()
 
@@ -16,13 +27,26 @@ def main() -> None:
     hue_light_id = service.register_device(hue_light)
     speaker_id = service.register_device(speaker)
     toilet_id = service.register_device(toilet)
+    await aio.gather(hue_light_id, speaker_id, toilet_id)
 
     # create a few programs
     wake_up_program = [
         Message(hue_light_id, MessageType.SWITCH_ON),
         Message(speaker_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
+        Message(
+            speaker_id,
+            MessageType.PLAY_SONG,
+            "Rick Astley - Never Gonna Give You Up",
+        ),
     ]
+
+    await run_sequence(
+        run_parallel(
+            service.send_msg(wake_up_program[0]),
+            service.send_msg(wake_up_program[1]),
+        ),
+        service.send_msg(wake_up_program[2]),
+    )
 
     sleep_program = [
         Message(hue_light_id, MessageType.SWITCH_OFF),
@@ -31,14 +55,21 @@ def main() -> None:
         Message(toilet_id, MessageType.CLEAN),
     ]
 
-    # run the programs
-    service.run_program(wake_up_program)
-    service.run_program(sleep_program)
+    await run_sequence(
+        run_parallel(
+            service.send_msg(sleep_program[0]),
+            service.send_msg(sleep_program[1]),
+        ),
+        run_sequence(
+            service.send_msg(sleep_program[2]),
+            service.send_msg(sleep_program[3]),
+        ),
+    )
 
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    main()
+    aio.run(main())
     end = time.perf_counter()
 
     print("Elapsed:", end - start)
